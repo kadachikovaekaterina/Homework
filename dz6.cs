49d8179e5642858b4d70e6093deabfb638458eef
kadachikovaekaterina <102862004+kadachikovaekaterina@users.noreply.github.com>
2022-05-06 23:08:42 +0400

    Add files via upload

---
+++ dz6.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2;  -1, -7, 567, 89, 223-> 3
+
+// Console.Write("Введите числа через пробел: ");
+
+// int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+// int Numbers(int[] array)
+// {
+//     int count = 0;
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0) count++;
+//     }
+//     return count;
+// }
+// Console.WriteLine(Numbers(array));
+
+
+
+// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+
+// y = k1 * x + b1;
+// y = k2 * x + b2;
+// x = (b2 - b1) / (k1 - k2);
+// y = (k1 * (b2 - b1) / (k1 - k2)) + b1;
+
+string X(int k1, int k2, int b1, int b2)
+{
+    string result = string.Empty;
+    int x = 0;
+    int y = 0;
+    x = (b2 - b1) / (k1 - k2);
+    y = (k1 * (b2 - b1) / (k1 - k2)) + b1;
+    result = $"({x}, {y})";
+    return result;
+}
+Console.WriteLine(X(2, -3, -3, 2));
